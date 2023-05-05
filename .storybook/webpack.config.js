2a019cb3f0fe54cab218a41fe65ddf5cf05c9f65
thor85 <35254400+thor85@users.noreply.github.com>
2023-05-05 10:18:07 +0200

    wip

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,11 +4,25 @@ module.exports = ({ config }) => {
     use: [
       {
         // loader: require.resolve('ts-loader'),
+        // loader: require.resolve("ts-loader")
         loader: 'ts-loader',
         // loader: require.resolve('awesome-typescript-loader'),
       },
     ],
+    // exclude: /node_modules/,
   });
+  // config.module.rules.push({
+  //   test: /\.(js|jsx)$/,
+  //   use: [
+  //     {
+  //       loader: require.resolve('babel-loader'),
+  //       // loader: require.resolve("ts-loader")
+  //       // loader: 'ts-loader',
+  //       // loader: require.resolve('awesome-typescript-loader'),
+  //     },
+  //   ],
+  //   exclude: [/bower_components/, /node_modules/, /styles/],
+  // });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
   };
